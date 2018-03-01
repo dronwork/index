cfd4d1d844bc36ea95c76c6c84f486d939a9333e
Andrei Keida <dronwork@outlook.com>
2018-03-01 22:01:24 +0200

    Upd files

--- a_main.rb
+++ a_main.rb
@@ -1,3 +1,24 @@
 # Linux Ubuntu 16.04
 # Ruby 2.4.1
 
+class Main
+  def func(index)
+    index.upto 10 do |x|
+      puts x.to_s
+    end
+
+    def meth(var)
+      var.each {|x| puts x}
+    end
+  end
+end
+
+class Func < Main
+  obj = Main.new
+  v = 5
+  blocks = ["bmv", "mers", "volvo", "toyota"]
+
+   obj.func(v)
+   obj.meth(blocks.reverse)
+
+end
