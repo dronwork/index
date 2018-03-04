1005e7a662bc3c05ef12fd9b7285cd28b2ef3c26
Andrei Keida <dronwork@outlook.com>
2018-03-04 15:36:49 +0200

    Set demo

--- a_main.rb
+++ a_main.rb
@@ -2,6 +2,12 @@
 # Ruby 2.4.1
 
 class Main
+  def self.classmeth(ax)
+    @ax = ax + 15
+    puts "Class Meth = #{@ax}"
+  end
+  @@inst = 60
+
   def func(index)
     index.upto 10 do |x|
       puts x.to_s
@@ -14,11 +20,15 @@ class Main
 end
 
 class Func < Main
-  obj = Main.new
+  @obj = Main.new
   v = 5
   blocks = ["bmv", "mers", "volvo", "toyota"]
 
-   obj.func(v)
-   obj.meth(blocks.reverse)
+   @obj.func(v)
+   @obj.meth(blocks.reverse)
+   Main.classmeth(5)
+   Func.classmeth(10)
+   Main.classmeth(0)
+
 
 end
